b213a3c9ca2df64b176ab7b16b1f8110cf1f94fc
susan <susan.ra.evans@gmail.com>
2016-10-16 13:41:06 -0700

    implemented person class

--- Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,26 +1,23 @@
 class Person
-  #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
+  attr_accessor :first_name, :last_name 
 
-  #have a class attribute called `people` that holds an array of objects
+  # ugh, ugh
   @@people = []
 
-  #have an `initialize` method to initialize each instance
-  def initialize(x,y)#should take 2 parameters for first_name and last_name
-    #assign those parameters to instance variables
-    #add the created instance (self) to people class variable
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @@people << self
   end
 
-  #have a `search` method to locate all people with a matching `last_name`
+  #find all people with a matching `last_name`
   def self.search(last_name)
-    #accept a `last_name` parameter
-    #search the `people` class attribute for instances with the same `last_name`
-    #return a collection of matching instances
+    @@people.select { |person| person.last_name == last_name}
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
-    #return a formatted string as `first_name(space)last_name`
+    "#{@first_name} #{@last_name}"
   end
 end
 
